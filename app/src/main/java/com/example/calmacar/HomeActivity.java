e75dc87e2bbc9b88c2a4cdd3ed3289f3e280c405
Mebarek Zouakh <zzmoubarek@gmail.com>
2023-03-04 18:02:21 +0100

    Home Activity Nav Drawer . Creating a nav drawer with a Menu & Header . Adding Nav drawer menu icons . Creating a custom toolbar for the Home activity . Creating Home activity fragments

--- app/src/main/java/com/example/calmacar/HomeActivity.java
+++ app/src/main/java/com/example/calmacar/HomeActivity.java
@@ -1,57 +1,135 @@
 package com.example.calmacar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     String userType;
-    TextView tv_userType;
-    Button btn_logout;
 
-    FirebaseAuth mAuth;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // Firebase
-        mAuth = FirebaseAuth.getInstance();
-
         // Hooks
-        tv_userType = findViewById(R.id.tv_userType);
-        btn_logout = findViewById(R.id.btn_logout);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        // Toolbar
+        setSupportActionBar(toolbar);
+
+        // Navigation Drawer Menu
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_nav_drawer, R.string.close_nav_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Initialize first fragment
+        getSupportFragmentManager().beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .add(R.id.frag_home, HomeFragment.newInstance("",""))
+                .commit();
+        navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
 
         // Intent Extras
+        // TODO remove this
         userType = getIntent().getStringExtra("EXTRA_USERTYPE");
 
-        // Initialize UI
-        tv_userType.setText("Welcome to " + userType + " Dashboard");
-
-        // Button Listeners
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logout();
-            }
-        });
-
     }
 
-    private void logout() {
-        mAuth.signOut();
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-        finish();
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                // show home fragment
+                // if profile fragment is already loaded, then return
+                if (getSupportFragmentManager().findFragmentById(R.id.frag_home) instanceof HomeFragment)
+                    break;
 
+                // Load profile fragment
+                getSupportFragmentManager().beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.frag_home, HomeFragment.newInstance("",""))
+                        .commit();
+                break;
+            case R.id.nav_trips:
+                // show trips fragment
+                // if profile fragment is already loaded, then return
+                if (getSupportFragmentManager().findFragmentById(R.id.frag_home) instanceof TripsFragment)
+                    break;
+
+                // Load profile fragment
+                getSupportFragmentManager().beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.frag_home, TripsFragment.newInstance("",""))
+                        .commit();
+                break;
+            case R.id.nav_wallet:
+                // show wallet fragment
+                // if profile fragment is already loaded, then return
+                if (getSupportFragmentManager().findFragmentById(R.id.frag_home) instanceof WalletFragment)
+                    break;
+
+                // Load profile fragment
+                getSupportFragmentManager().beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.frag_home, WalletFragment.newInstance("",""))
+                        .commit();
+                break;
+            case R.id.nav_profile:
+                // show profile fragment
+                // if profile fragment is already loaded, then return
+                if (getSupportFragmentManager().findFragmentById(R.id.frag_home) instanceof ProfileFragment)
+                    break;
+
+                // Load profile fragment
+                getSupportFragmentManager().beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.frag_home, ProfileFragment.newInstance("",""))
+                        .commit();
+                break;
+            case R.id.nav_logout:
+                // Logout user
+                Auth.getInstance().logout();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+
+        return true;
+    }
 }
