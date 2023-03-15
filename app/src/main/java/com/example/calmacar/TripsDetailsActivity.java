f6383b1f3ca99cba6932fc97d351d0a4d560cbdd
Aissam <aissam@gmail.com>
2023-03-15 18:40:00 +0100

    creation activity_trips_details et mettre à jour listview_search_result

---
+++ app/src/main/java/com/example/calmacar/TripsDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.calmacar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TripsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trips_details);
+    }
+}
