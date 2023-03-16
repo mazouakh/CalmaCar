b6cc49419d3a3f03d6b89d6cdb9bf33a097586bc
Mebarek Zouakh <zzmoubarek@gmail.com>
2023-03-16 17:58:31 +0100

    Activity Successful Reservation . just first creation

---
+++ app/src/main/java/com/example/calmacar/passenger/SuccessfulReservationActivity.java
@@ -0,0 +1,17 @@
+package com.example.calmacar.passenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.calmacar.R;
+
+public class SuccessfulReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successful_reservation);
+    }
+
+}
